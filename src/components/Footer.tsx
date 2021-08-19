e376dd00c8327a5e6b4f86fdc8a2127aff0d8d9d
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2021-08-19 18:09:27 -0400

    feat: Footer.tsx added

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from "react";
+
+const Footer = (): ReactElement => (
+	<footer className="container absolute bottom-0 max-w-none text-center">
+		<h6>I'm here to stay (Footer)</h6>
+	</footer>
+);
+
+export default Footer;
